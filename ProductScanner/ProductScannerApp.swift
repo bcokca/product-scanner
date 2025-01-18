5cf62d86673e0bc365b52384c57b5248ac04739f
Burhan Cokca <burhan.cokca@doordash.com>
2025-01-17 19:06:02 -1000

    Initial Commit

---
+++ ProductScanner/ProductScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductScannerApp.swift
+//  ProductScanner
+//
+//  Created by Burhan COKCA on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ProductScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProductScannerTests/ProductScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProductScannerTests.swift
+//  ProductScannerTests
+//
+//  Created by Burhan COKCA on 1/17/25.
+//
+
+import Testing
+@testable import ProductScanner
+
+struct ProductScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
